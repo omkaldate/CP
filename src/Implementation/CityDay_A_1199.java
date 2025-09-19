e1764c85324e0e34df44edc898e5610807c28e24
Om Kaldate <omkaldate23@gmail.com>
2025-09-19 23:37:30 +0530

    dsa

---
+++ src/Implementation/CityDay_A_1199.java
@@ -0,0 +1,16 @@
+package Implementation;
+
+import java.util.Scanner;
+
+public class CityDay_A_1199 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+
+        int[]arr = new int[n];
+        for(int i=0; i<n; i++) arr[i] = sc.nextInt();
+
+    }
+}
